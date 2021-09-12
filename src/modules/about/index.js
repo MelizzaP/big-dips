3b3a8384b842ffa6c4d546b05351ee6b62f771a6
Melissa Patterson <melissa.patterson.va@gmail.com>
2021-09-12 14:02:55 -0500

    Update About to include openning paragraph

--- src/modules/about/index.js
+++ src/modules/about/index.js
@@ -6,6 +6,10 @@ export default function About() {
   return (
     <div className="my-32 mx-16 flex flex-col items-center gap-y-6">
       <h1 className="text-3xl underline">Our Story</h1>
+      <p className="text-xl text-center">
+        Hi! Welcome to BigDips! We are Laura & Melissa, partners in life and also co-founders of
+        BigDips. Here's a little bit about our story:
+      </p>
       <img src={picture} alt="Melissa and Laura" className="max-h-64 rounded-md drop-shadow" />
       <p className="text-xl text-center">
         This company came to be because of Melissa always wanting a Vitamix. Laura thought they were
