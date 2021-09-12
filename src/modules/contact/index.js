0815b5066de95731ea844d7036eb57d78aa9e5e2
Melissa Patterson <melissa.patterson.va@gmail.com>
2021-09-12 13:53:47 -0500

    Adjust spacing in contact us

--- src/modules/contact/index.js
+++ src/modules/contact/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className="m-32 flex flex-col gap-y-6">
+    <div className="my-24 mx-6 md:m-32 flex flex-col gap-y-6 text-center">
       <h1 className="text-3xl">Contact Us</h1>
       <h2 className="text-xl">Want to place an order or learn more about Big Dips?</h2>
       <a href="mailto:contact@bigdips.co">
