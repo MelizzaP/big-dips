e9ddfc34a3e51cf12c9ea1ba90114e82216a885f
Melissa Patterson <melissa.patterson.va@gmail.com>
2021-09-21 20:16:18 -0500

    About us copy update

--- src/modules/about/index.js
+++ src/modules/about/index.js
@@ -7,18 +7,19 @@ export default function About() {
     <div className="my-32 mx-16 flex flex-col items-center gap-y-6">
       <h1 className="text-3xl underline">Our Story</h1>
       <p className="text-xl text-center">
-        Hi! Welcome to BigDips! We are Laura & Melissa, partners in life and also co-founders of
-        BigDips. Here's a little bit about our story:
+        Hi! Welcome! BigDips is the brainchild of Laura & Melissa, partners in life, and co-founders
+        of BigDips.
       </p>
       <img src={picture} alt="Melissa and Laura" className="max-h-64 rounded-md drop-shadow" />
       <p className="text-xl text-center">
-        This company came to be because of Melissa always wanting a Vitamix. Laura thought they were
-        too expensive and dragged her feet on the purchase but during the covid lockdown of 2020,
-        Laura decided to finally surprise Melissa with the Queen of kitchen accessories. From that
-        point our life changed. Melissa was a wizard, making dip after dip that were healthy, vegan,
-        and highly sought after in our building. After a while, we realized, we might just have
-        something! Thus, BigDips was born.
+        Melissa always dreamed of having a Vitamix. Laura thought they were too expensive and
+        overrated. But then, during the 2020 COVID lockdown, Laura decided to finally give in and
+        surprise Melissa with the queen of all kitchen accessories. From that point, their life
+        changed. Melissa was a wizard, making dip after dip that were delicious, healthy, vegan, and
+        highly sought after in the building. After a while, they realized, "we might just have
+        something!" Thus, BigDips was born.
       </p>
+      <p className="text-xl text-center">Hope you enjoy the dips as much as our neighbors.</p>
     </div>
   )
 }
